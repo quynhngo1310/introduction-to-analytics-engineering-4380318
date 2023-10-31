217e440e54fd3a3e944ab6f0d5d895823219761a
quynhngo1310 <149533293+quynhngo1310@users.noreply.github.com>
2023-10-31 15:04:21 -0500

    Update make-pr-here.sql
    added in customers and # of states metric

--- second-challenge/make-pr-here.sql
+++ second-challenge/make-pr-here.sql
@@ -6,7 +6,9 @@ create view product_perf as
         , p.Price as Product_Price
         , sum(o.Quantity) as Total_Product_Sold
         , count(o.Order_Id) as Total_Product_Orders
-
+        , count( distinct c.state) as total_state
 from products p
 join orders o
     on o.product_id = p.product_id
+JOIN customers c 
+    on c.cus_id = o.cus_id
